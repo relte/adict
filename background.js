6f6bf31ea75e59cc8d06f6aad513dc29e5bfd88f
relte <9462146+relte@users.noreply.github.com>
2018-09-19 19:02:19 +0200

    Refactor saving the default dictionary URL

--- background.js
+++ background.js
@@ -1,17 +1,11 @@
-browser.storage.local.get('dictionaryUrl')
-    .then(function (data) {
-        if (typeof data.dictionaryUrl === 'undefined') {
-            browser.storage.local.set({
-                dictionaryUrl: 'https://www.dictionary.com/browse/%phrase%?s=t&addon=true'
-            });
-        }
-    })
-;
-
 browser.runtime.onInstalled.addListener(function (details) {
     if (details.reason === 'update') {
         browser.tabs.create({
             url: 'update_page/update_page.html'
+        });
+    } else if (details.reason === 'install') {
+        browser.storage.local.set({
+            dictionaryUrl: 'https://www.dictionary.com/browse/%phrase%?s=t&addon=true'
         });
     }
 });
