cffac1b9443fb066451f3660758106c16dd7f45c
Krzysztof Lament <kontakt@krzysztoflament.pl>
2018-09-21 19:50:23 +0200

    Increase loading speed, adjust run script

--- background.js
+++ background.js
@@ -9,3 +9,39 @@ browser.runtime.onInstalled.addListener(details => {
         });
     }
 });
+
+browser.webRequest.onBeforeRequest.addListener(
+    blockRequest,
+    {
+        urls: [
+            "*://js-sec.indexww.com/*",
+            "*://*.amazon-adsystem.com/*",
+            "*://cdn.heapanalytics.com/*",
+            "*://*.google.com/*",
+            "*://*.google.pl/*",
+            "*://*.googletagservices.com/*",
+            "*://*.google-analytics.com/*",
+            "*://*.googletagmanager.com/*",
+            "*://*.googlesyndication.com/*",
+            "*://*.facebook.com/*",
+            "*://*.facebook.net/*",
+            "*://*.quantserve.com/*",
+            "*://*.pubmatic.com/*",
+            "*://*.bounceexchange.com/*"
+        ]
+    },
+    ['blocking']
+);
+
+function blockRequest(requestDetails) {
+    let merriamWebsterExp = new RegExp(/.*merriam-webster\.com.*addon=true.*/);
+    let dictionaryComExp = new RegExp(/.*dictionary\.com.*addon=true.*/);
+    let dikiExp = new RegExp(/.*diki\.pl.*addon=true.*/);
+    if (requestDetails.originUrl.match(merriamWebsterExp) ||
+        requestDetails.originUrl.match(dictionaryComExp) ||
+        requestDetails.originUrl.match(dikiExp)
+    ) {
+        console.log(requestDetails.url);
+        return {cancel: true};
+    }
+}
