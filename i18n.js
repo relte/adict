c9786c1ce3c6f16280cfc730ecc5cdd2bd7891df
relte <9462146+relte@users.noreply.github.com>
2018-09-21 21:07:58 +0200

    Refactor

--- i18n.js
+++ i18n.js
@@ -1,4 +1,9 @@
-let elements = document.querySelectorAll('[data-i18n]');
-for(let i = 0; i < elements.length; i++) {
-    elements[i].innerHTML = browser.i18n.getMessage(elements[i].dataset.i18n);
+function translatePage() {
+    let elements = document.querySelectorAll('[data-i18n]');
+
+    for(let i = 0; i < elements.length; i++) {
+        elements[i].innerHTML = browser.i18n.getMessage(elements[i].dataset.i18n);
+    }
 }
+
+translatePage();
