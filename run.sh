7ebf73cca7bba3d1ad7ac9d886a0a1bd1f66b01f
relte <9462146+relte@users.noreply.github.com>
2018-09-21 14:26:43 +0200

    Add run script for Firefox

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+web-ext run --start-url https://medium.com/s/story/were-primed-to-be-addicted-to-social-media-b6b31cfc4099 \
+            --start-url https://english.stackexchange.com/questions/465198/a-word-for-an-object-that-doesnt-fit-in \
+            --start-url about:debugging
