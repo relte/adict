92b3b09d2afc8032c8aec2eb01fe678c009f3f50
Krzysztof Lament <kontakt@krzysztoflament.pl>
2018-12-08 15:09:28 +0100

    Use translation only if found

--- i18n.js
+++ i18n.js
@@ -2,7 +2,10 @@ function translatePage() {
     let elements = document.querySelectorAll('[data-i18n]');
 
     for(let i = 0; i < elements.length; i++) {
-        elements[i].innerHTML = browser.i18n.getMessage(elements[i].dataset.i18n);
+        let message = browser.i18n.getMessage(elements[i].dataset.i18n);
+        if (message) {
+            elements[i].innerHTML = message;
+        }
     }
 }
 
