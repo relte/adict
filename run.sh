5f452eeafe3624d0dd347d4e7657997b3e748935
Krzysztof Lament <kontakt@krzysztoflament.pl>
2018-12-10 22:52:27 +0100

    Set Firefox interface language in the run script

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 web-ext run --start-url https://english.stackexchange.com/questions/465198/a-word-for-an-object-that-doesnt-fit-in \
-            --start-url about:debugging
+            --start-url about:debugging \
+            --pref intl.locale.requested=en
